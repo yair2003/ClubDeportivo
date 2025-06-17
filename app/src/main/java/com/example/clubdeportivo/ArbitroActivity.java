eb932b4a5ae89fa6c916c82e686f2b8eaca064a1
Yair Joaquin Aguero Gamarra <U22219929@utp.edu.pe>
2025-06-16 23:42:12 -0500

    botones

--- app/src/main/java/com/example/clubdeportivo/ArbitroActivity.java
+++ app/src/main/java/com/example/clubdeportivo/ArbitroActivity.java
@@ -1,6 +1,8 @@
 package com.example.clubdeportivo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,41 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ArbitroActivity extends AppCompatActivity {
 
+    Button btnPartidos, btnChat, btnHistorial, btnReportar, btnPerfil;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.arbitro);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Enlazar botones con sus IDs
+        btnPartidos = findViewById(R.id.btnPartidos);
+        btnChat = findViewById(R.id.btnChat);
+        btnHistorial = findViewById(R.id.btnHistorial);
+        btnReportar = findViewById(R.id.btnReportar);
+        btnPerfil = findViewById(R.id.btnPerfil);
+
+        // Acciones de cada botón
+        btnPartidos.setOnClickListener(v ->
+                startActivity(new Intent(ArbitroActivity.this, PartidosActivity.class)));
+
+        btnChat.setOnClickListener(v ->
+                startActivity(new Intent(ArbitroActivity.this, ChatActivity.class)));
+
+        btnHistorial.setOnClickListener(v ->
+                startActivity(new Intent(ArbitroActivity.this, HistorialActivity.class)));
+
+        btnReportar.setOnClickListener(v ->
+                startActivity(new Intent(ArbitroActivity.this, ReportarProblemaActivity.class)));
+
+        btnPerfil.setOnClickListener(v ->
+                startActivity(new Intent(ArbitroActivity.this, PerfilAjustesActivity.class)));
     }
 }
