857a14f99bbf4d31eb993a8207932830bbdee04a
Yair Joaquin Aguero Gamarra <U22219929@utp.edu.pe>
2025-06-18 21:55:07 -0500

    ok

---
+++ app/src/main/java/com/example/clubdeportivo/PerfilAjustesActivity.java
@@ -0,0 +1,12 @@
+package com.example.clubdeportivo;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PerfilAjustesActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.perfil_ajustes);
+    }
+}
